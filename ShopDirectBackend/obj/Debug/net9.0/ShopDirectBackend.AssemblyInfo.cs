4f8182660f0e4ea4f0b1dfa6fdec945f117c77d3
ngothoiquang24042004@gmail.com <ngothoiquang24042004@gmail.com>
2026-08-25 08:22:37 +0700

    up

--- ShopDirectBackend/obj/Debug/net9.0/ShopDirectBackend.AssemblyInfo.cs
+++ ShopDirectBackend/obj/Debug/net9.0/ShopDirectBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopDirectBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d8593c87ce17f33559bba6cbe499a8345af779b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caa859e934ce446629eeee428e53531f92a8ea93")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopDirectBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopDirectBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
